ee931286baa7ff5c87fc13f85489320b6567171d
alessandroryo <anan0002@hz.nl>
2022-02-11 19:26:05 +0100

    Finished week 1

---
+++ app/Models/Grade.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Grade extends Model
+{
+    use HasFactory;
+
+    public function addResult($number) {
+        if ($number > $this->best_grade) {
+            $this->best_grade = $number;
+            if ($this->best_grade >= $this->lowest_passing_grade) {
+                $this->passed_at = now();
+            }
+        }
+    }
+}
